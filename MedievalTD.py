73252a2b7718d565cb23e6e3d47b01737927a107
The-Techno-bug <70822971+The-Techno-bug@users.noreply.github.com>
2026-06-01 10:38:47 -0400

    Fixed projectile spawning and created proper range

--- MedievalTD.py
+++ MedievalTD.py
@@ -6,6 +6,8 @@ import math
 pygame.init()
 screen = pygame.display.set_mode((768,768))
 rangeSurface = pygame.surface.Surface((768,768))
+path = pygame.image.load("Images/path.png")
+path = pygame.transform.scale(path,(768,768))
 
 #Create enemy path
 
@@ -69,24 +71,25 @@ class Tower:
             newImage = pygame.transform.rotate(self.shootImage,self.angle)
         else:
             newImage = pygame.transform.rotate(self.image,self.angle)
-        screen.blit(newImage,(self.x,self.y))
+        newRect = newImage.get_rect(center=(self.x,self.y))
+        screen.blit(newImage,newRect)
         for projectile in self.projectiles:
             projectile.drawSelf()
         if self.selected:
-            pygame.draw.circle(rangeSurface,(255,255,255),(self.x+8,self.y+8),self.range)
-            pygame.draw.circle(screen,(255,255,255),(self.x+8,self.y+8),self.range,2)
+            pygame.draw.circle(rangeSurface,(255,255,255),(self.x,self.y),self.range)
+            pygame.draw.circle(screen,(255,255,255),(self.x,self.y),self.range,2)
 
     def checkClick(self,mousePos):
-        towerRect = pygame.Rect(self.x,self.y,8,8)
+        towerRect = pygame.Rect(self.x-24,self.y-24,48,48)
         if towerRect.collidepoint(mousePos[0],mousePos[1]):
             self.selected = not self.selected
     
     def shoot(self,angle):
-        self.angle = angle
+        self.angle = angle + 180
         newAngle = math.radians(angle)
-        deltaX = 16*math.cos(newAngle)
-        deltaY = 16*math.sin(newAngle)
-        self.projectiles.append(Projectile(self.x+deltaX,self.y+deltaY,self.spawnedProjectileSpeed,newAngle,self.spawnedProjectileImage,self.rotateProjectileImage,self.projectileDamage,self.projectilePierce))
+        deltaX = 24*math.sin(newAngle)
+        deltaY = 24*math.cos(newAngle)
+        self.projectiles.append(Projectile(self.x+deltaX,self.y+deltaY,self.x,self.y,self.spawnedProjectileSpeed,newAngle,self.spawnedProjectileImage,self.rotateProjectileImage,self.projectileDamage,self.projectilePierce,self.range))
         self.isShooting = True
         self.shotTime = currentTime
 
@@ -101,34 +104,44 @@ class Tower:
             self.isShooting = False
 
 class Cannon(Tower):
-    def __init__(self, x, y, angle):
-        super().__init__(x, y, 30, 50, angle, False, "Images/cannon_idle.png","Images/cannon_shoot.png",200,"Images/cannon_projectile.png",False,1,5)
+    def __init__(self, x, y):
+        super().__init__(x, y, 30, 200, 0, False, "Images/cannon_idle.png","Images/cannon_shoot.png",200,"Images/cannon_projectile.png",False,1,5)
+
+class Ballista(Tower):
+    def __init__(self, x, y):
+        super().__init__(x, y, 50, 400, 0, False, "Images/ballista_idle.png", "Images/ballista_shoot.png", 500, "Images/cannon_projectile.png", False, 3, 8)
 
 class Projectile:
-    def __init__(self,x,y,vel,angle,imgPath,toRotate,damage,pierce):
+    def __init__(self,x,y,parentX,parentY,vel,angle,imgPath,toRotate,damage,pierce,range):
         self.x = x
         self.y = y
+        self.parentX = parentX
+        self.parentY = parentY
         self.velX = vel*math.sin(angle)
         self.velY = vel*math.cos(angle)
         self.image = pygame.image.load(imgPath)
         self.image = pygame.transform.scale(self.image,(48,48))
         if toRotate:
             self.image = pygame.transform.rotate(self.image,angle+180)
+        self.damage = damage
+        self.pierce = pierce
+        self.range = range
     
     def drawSelf(self):
-        screen.blit(self.image,(self.x,self.y))
+        newRect = self.image.get_rect(center=(self.x,self.y))
+        screen.blit(self.image,newRect)
 
     def tick(self):
         self.x += self.velX/60
         self.y += self.velY/60
 
     def despawnCheck(self):
-        if self.x > 300 or self.x < -50 or self.y > 300 or self.y < -50:
+        if ((self.parentX-self.x)**2 + (self.parentY-self.y)**2)**0.5 >= self.range:
             return True
         return False
 
 
-tower1 = Cannon(100,100,30)
+tower1 = Ballista(150,150)
 rangeSurface.set_alpha(80)
 
 currentTowers = [tower1]
@@ -136,9 +149,11 @@ currentEnemies = []
 
 clock = pygame.time.Clock()
 lastTime = 0
+a = 0
 while True:
     screen.fill((0,0,0))
     rangeSurface.fill((0,0,0))  #Fill surfaces
+    screen.blit(path,(0,0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -149,7 +164,10 @@ while True:
     
     currentTime = pygame.time.get_ticks()
     if currentTime - lastTime >= 2000:
-        tower1.shoot(random.randint(0,360))
+        a += 30
+        a %= 360
+        tower1.shoot(a)
+        print(a)
         lastTime = currentTime
     
     for tower in currentTowers:
