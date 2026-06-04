1a584d799333aa2c053eb6aeb764f2726eb19115
The-Techno-bug <70822971+The-Techno-bug@users.noreply.github.com>
2026-06-04 08:51:50 -0400

    Fixed gamePath and added speed and keyboard functionality

--- MedievalTD.py
+++ MedievalTD.py
@@ -12,17 +12,22 @@ path = pygame.transform.scale(path,(768,768))
 menu = pygame.image.load("Images/side_menu.png")
 menu = pygame.transform.scale(menu,(144,768))
 
+speed1Image = pygame.image.load("Images/speed_normal.png")
+speed1Image = pygame.transform.scale(speed1Image,(48,48))
+speed2Image = pygame.image.load("Images/speed_fast.png")
+speed2Image = pygame.transform.scale(speed2Image,(48,48))
+
 #Create enemy path
 gamePath = []
 gamePath += [(360,y)for y in range(0,505)]
 gamePath += [(x,504)for x in range(360,553)]
-gamePath += [(552,y)for y in range(168,103,-1)]
-gamePath += [(x,104)for x in range(184,23,-1)]
-gamePath += [(24,y)for y in range(104,39,-1)]
-gamePath += [(x,40)for x in range(24,91)]
-gamePath += [(90,y)for y in range(40,201)]
-gamePath += [(x,200)for x in range(90,139)]
-gamePath += [(138,y)for y in range(200,256)]
+gamePath += [(552,y)for y in range(504,307,-1)]
+gamePath += [(x,308)for x in range(552,71,-1)]
+gamePath += [(72,y)for y in range(308,119,-1)]
+gamePath += [(x,120)for x in range(72,271)]
+gamePath += [(270,y)for y in range(120,601)]
+gamePath += [(x,600)for x in range(270,415)]
+gamePath += [(414,y)for y in range(600,756)]
 
 #Create tile system - 16px*16px tiles
 
@@ -52,43 +57,55 @@ class Enemy:
         self.x = x
         self.y = y
         self.health = health
+        self.maxHealth = health
         self.speed = speed   
         self.pathIndex = 0
         #Loads basic Attributes for the Enemy class
         self.image = pygame.image.load(imgPath)
+        self.image = pygame.transform.scale(self.image,(48,48))
 
     def drawSelf(self):
-        screen.blit(self.image,(self.x,self.y))
-
+        imageRect = self.image.get_rect(center=(self.x,self.y))
+        screen.blit(self.image,imageRect)
+        pygame.draw.rect(screen,(15,15,15),(imageRect.center[0]-24,imageRect.center[1]-30,48,5))
+        pygame.draw.rect(screen,(255,0,0),(imageRect.center[0]-24,imageRect.center[1]-30,self.health/48,5))
+    
+    def move(self):
+        self.pathIndex += 1
+        self.x = gamePath[self.pathIndex][0]
+        self.y = gamePath[self.pathIndex][1]
+        if speed == 2:
+            self.pathIndex += 1
+            self.x = gamePath[self.pathIndex][0]
+            self.y = gamePath[self.pathIndex][1]    
 
 class Tower:
-    def __init__(self,x,y,atkSpeed,range,angle,selected,imgPath,shootImgPath,spawnedProjectileSpeed,spawnedProjectileImage,rotateProjectileImage,projectilePierce,projectileDamage):
+    def __init__(self,x,y,atkSpeed,range,angle,selected,shootImgPathList,spawnedProjectileSpeed,spawnedProjectileImage,rotateProjectileImage,projectilePierce,projectileDamage,animationTime):
         self.x = x
         self.y = y
         self.atkSpeed = atkSpeed
         self.range = range
         self.angle = angle
         self.selected = selected
-        self.image = pygame.image.load(imgPath)
-        self.image = pygame.transform.scale(self.image,(48,48))
-        self.shootImage = pygame.image.load(shootImgPath)
-        self.shootImage = pygame.transform.scale(self.shootImage,(48,48))
+        self.shootingImages = []
+        for imagePath in shootImgPathList:
+            image = pygame.image.load(imagePath)
+            image = pygame.transform.scale(image,(48,48))
+            self.shootingImages.append(image)
         self.projectiles = []
         self.spawnedProjectileSpeed = spawnedProjectileSpeed
         self.spawnedProjectileImage = spawnedProjectileImage
         self.rotateProjectileImage = rotateProjectileImage
         self.projectilePierce = projectilePierce
-        self.isShooting = False
-        self.lastShotTime = 0
+        self.shootingPhase = 0
         self.projectileDamage = projectileDamage
         self.shotTime = 0
-        self.animationTime = 100
+        self.animationTime = animationTime
+        self.phaseTime = 0
+        self.isCycled = False
 
     def drawSelf(self):
-        if self.isShooting:
-            newImage = pygame.transform.rotate(self.shootImage,self.angle)
-        else:
-            newImage = pygame.transform.rotate(self.image,self.angle)
+        newImage = pygame.transform.rotate(self.shootingImages[-self.shootingPhase],self.angle)
         newRect = newImage.get_rect(center=(self.x,self.y))
         screen.blit(newImage,newRect)
         for projectile in self.projectiles:
@@ -108,8 +125,10 @@ class Tower:
         deltaX = 24*math.sin(newAngle)
         deltaY = 24*math.cos(newAngle)
         self.projectiles.append(Projectile(self.x+deltaX,self.y+deltaY,self.x,self.y,self.spawnedProjectileSpeed,angle,self.spawnedProjectileImage,self.rotateProjectileImage,self.projectileDamage,self.projectilePierce,self.range))
-        self.isShooting = True
+        self.shootingPhase = len(self.shootingImages) - 1
         self.shotTime = currentTime
+        self.phaseTime = currentTime
+        self.isCycled = False
 
     def projChecks(self):
         for i,projectile in enumerate(self.projectiles):
@@ -118,21 +137,25 @@ class Tower:
             projectile.tick()
 
     def updateAnimation(self):
-        if currentTime-self.shotTime >= self.animationTime:
-            self.isShooting = False
+        if currentTime-self.phaseTime >= self.animationTime/speed and not self.isCycled:
+            self.shootingPhase -= 1
+            self.phaseTime = currentTime
+        if self.shootingPhase < 0:
+            self.shootingPhase = 0
+            self.phaseTime = currentTime
+            self.isCycled = True
+
     def attack(self):
-        if currentTime-self.shotTime >= self.atkSpeed:
+        if currentTime-self.shotTime >= self.atkSpeed/speed:
             self.shoot(random.randint(0,360))
-
-
 
 class Cannon(Tower):
     def __init__(self, x, y):
-        super().__init__(x, y, 3000, 300, 0, False, "Images/cannon_idle.png","Images/cannon_shoot.png",200,"Images/cannon_projectile.png",False,1,5)
+        super().__init__(x, y, 3000, 300, 0, False,["Images/cannon_idle.png","Images/cannon_shoot.png"],200,"Images/cannon_projectile.png",False,1,5,100)
 
 class Ballista(Tower):
     def __init__(self, x, y):
-        super().__init__(x, y, 333, 400, 0, False, "Images/ballista_idle.png", "Images/ballista_shoot.png",500, "Images/ballista_projectile.png", True, 3, 8)
+        super().__init__(x, y, 300, 400, 0, False,["Images/ballista1.png","Images/ballista2.png","Images/ballista3.png","Images/ballista4.png"],500, "Images/ballista_projectile.png", True, 3, 8, 70)
 
 class Projectile:
     def __init__(self,x,y,parentX,parentY,vel,angle,imgPath,toRotate,damage,pierce,range):
@@ -155,8 +178,8 @@ class Projectile:
         screen.blit(self.image,newRect)
 
     def tick(self):
-        self.x += self.velX/60
-        self.y += self.velY/60
+        self.x += speed*self.velX/60
+        self.y += speed*self.velY/60
 
     def despawnCheck(self):
         if ((self.parentX-self.x)**2 + (self.parentY-self.y)**2)**0.5 >= self.range:
@@ -164,17 +187,19 @@ class Projectile:
         return False
 
 
-tower1 = Cannon(150,150)
 rangeSurface.set_alpha(80)
 placementSurface.set_alpha(60)
 
+enemy1 = Enemy(300,300,100,10,"Images/enemy.png")
+enemy1.health = 300
+
 currentTowers = []
-currentTowers.append(tower1)
 currentEnemies = []
+currentEnemies.append(enemy1)
 
 cannonImage = pygame.image.load("Images/cannon_idle.png")
 cannonImage = pygame.transform.scale(cannonImage,(48,48))
-ballistaImage = pygame.image.load("Images/ballista_idle.png")
+ballistaImage = pygame.image.load("Images/ballista1.png")
 ballistaImage = pygame.transform.scale(ballistaImage,(48,48))
 
 def towerShop():
@@ -194,9 +219,13 @@ def placeTower(x,y):
 
 placing = "none"
 
+EnemyMove = pygame.event.custom_type()
+pygame.time.set_timer(EnemyMove,20)
+
 clock = pygame.time.Clock()
 lastTime = 0
 a = 0
+speed = 1
 while True:
     screen.fill((0,0,0))
     rangeSurface.fill((0,0,0))
@@ -239,24 +268,51 @@ while True:
                     if placing == "ballista":
                         placing = "none"
                     else:
-                        placing = "ballista"
-                                
+                        placing = "ballista"   
 
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 placing = "none"
+                for tower in currentTowers:
+                    tower.selected = False
+            elif event.key == pygame.K_q:
+                if placing != "cannon":
+                    placing = "cannon"
+                else:
+                    placing = "none"
+            elif event.key == pygame.K_w:
+                if placing != "ballista":
+                    placing = "ballista"
+                else:
+                    placing = "none"
+            elif event.key == pygame.K_SPACE:
+                if speed == 1:
+                    speed = 2
+                else:
+                    speed = 1
+        
+        elif event.type == EnemyMove:
+            for enemy in currentEnemies:
+                enemy.move()
     currentTime = pygame.time.get_ticks()
     
-    
+
     for tower in currentTowers:
         tower.updateAnimation()
         tower.projChecks()
         tower.drawSelf()
         tower.attack()
-            
+    
+    for enemy in currentEnemies:
+        enemy.drawSelf()
+
     screen.blit(rangeSurface,(0,0))
     screen.blit(placementSurface,(0,0))
     towerShop()
+    if speed == 1:
+        screen.blit(speed1Image,(672,720))
+    else:
+        screen.blit(speed2Image,(672,720))
     pygame.display.update()
     clock.tick(60)
     
